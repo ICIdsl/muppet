de7aefe0a95457321f3530b22ae063bf22367710
Diederik Vink <dav114@ic.ac.uk>
2019-07-09 11:58:00 +0100

    updated param_parser to inherit from src

--- param_parser.py
+++ param_parser.py
@@ -3,59 +3,12 @@ from __future__ import print_function
 import argparse
 import configparser as cp
 
-class Params() : 
+import src.param_parser as ppSrc
+
+class Params(ppSrc.Params): 
     def __init__(self, config_file) : 
-        # attributes read in from config file 
-        self.dataset = config_file.get('dataset', 'dataset')
-        self.data_location = config_file.get('dataset', 'dataset_location')
-
-        self.arch = config_file.get('cnn', 'architecture')        
-        self.depth = config_file.get('cnn', 'depth')       
-        self.cardinality = config_file.get('cnn', 'cardinality')
-        self.widen_factor = config_file.get('cnn', 'widen_factor')
-        self.growth_rate = config_file.get('cnn', 'growth_rate')
-        self.compression_rate = config_file.get('cnn', 'compression_rate')
-
-        # self.start_epoch = config_file.getint('training_hyperparameters', 'start_epoch')
-        self.printOnly = config_file.getboolean('training_hyperparameters', 'print_only')
-        self.epochs = config_file.getint('training_hyperparameters', 'total_epochs')
-        self.train_batch = config_file.getint('training_hyperparameters', 'train_batch')
-        self.test_batch = config_file.getint('training_hyperparameters', 'test_batch') 
-        self.lr = config_file.getfloat('training_hyperparameters', 'learning_rate')
+        super().__init__(config_file)
         self.minLR = config_file.getfloat('training_hyperparameters', 'min_lr')
-        self.dropout = config_file.getfloat('training_hyperparameters', 'dropout_ratio')
-        self.gamma = config_file.getfloat('training_hyperparameters', 'gamma')
-        self.momentum = config_file.getfloat('training_hyperparameters', 'momentum') 
-        self.weight_decay = config_file.getfloat('training_hyperparameters', 'weight_decay') 
-        self.mo_schedule = [self.__to_num(i) for i in config_file.get('training_hyperparameters', 'momentum_schedule').split()]
-        self.lr_schedule = [self.__to_num(i) for i in config_file.get('training_hyperparameters', 'lr_schedule').split()]
-        self.trainValSplit = config_file.getfloat('training_hyperparameters', 'train_val_split')
-        
-        self.sub_classes = config_file.get('pruning_hyperparameters', 'sub_classes').split() 
-
-        self.manual_seed = config_file.getint('pytorch_parameters', 'manual_seed')
-        self.workers = config_file.getint('pytorch_parameters', 'data_loading_workers')
-        self.gpu_id = config_file.get('pytorch_parameters', 'gpu_id')
-        self.pretrained = config_file.get('pytorch_parameters', 'pretrained')        
-        self.checkpoint = config_file.get('pytorch_parameters', 'checkpoint_path')
-        self.test_name = config_file.get('pytorch_parameters', 'test_name')
-        self.resume = config_file.getboolean('pytorch_parameters', 'resume')
-        self.branch = config_file.getboolean('pytorch_parameters', 'branch')
-        self.evaluate = config_file.getboolean('pytorch_parameters', 'evaluate')
-        self.tee_printing = config_file.get('pytorch_parameters', 'tee_printing')
-        self.enableTbx = config_file.get('pytorch_parameters', 'enable_tbx')
-
-        # attributes used internally
-        self.use_cuda = True
-        self.start_epoch = 0 
-        self.curr_epoch = 0 
-        self.train_loss = 0 
-        self.train_top1 = 1
-        self.train_top5 = 1
-        self.test_loss = 0 
-        self.test_top1 = 1
-        self.test_top5 = 1
-        self.bestValidLoss = 0.0
 
         # muppet attributes 
         self.runMuppet = config_file.getboolean('muppet_hyperparameters', 'run_muppet')
@@ -79,15 +32,6 @@ class Params() :
         self.sub_classes = []
         self.threshold = -1
         
-    def get_state(self) : 
-        return self.__dict__
-
-    def __to_num(self, x) : 
-        try : 
-            return int(x) 
-        except ValueError: 
-            return float(x) 
-
 def parse_command_line_args() : 
     parser = argparse.ArgumentParser(description='PyTorch Pruning')
 
